4315aa6a26b9482f8d3491e808c702b8b68a7c58
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-03-04 14:04:20 +0000

    VIT customer entity

---
+++ customer-service/src/main/java/com/customer/customer_service/entities/Customer.java
@@ -0,0 +1,40 @@
+package com.customer.customer_service.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Last name cannot be empty")
+    private String lastName;
+
+    @NotBlank(message = "First name cannot be empty")
+    private String firstName;
+
+    @NotNull(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotNull(message = "Address is required")
+    private String address;
+
+    @NotNull(message = "Phone number is required")
+    private String phone;
+}
