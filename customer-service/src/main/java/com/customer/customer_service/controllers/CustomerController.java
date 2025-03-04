33b9054241a6b3041c290140172e5bad6463c82e
TERMOUSSI-LAMIAA <lamiaa3105@gmail.com>
2025-03-04 14:20:40 +0000

    VIT customer controller

---
+++ customer-service/src/main/java/com/customer/customer_service/controllers/CustomerController.java
@@ -0,0 +1,50 @@
+package com.customer.customer_service.controllers;
+
+import com.customer.customer_service.entities.Customer;
+import com.customer.customer_service.service.inter.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.addCustomer(customer);
+        return ResponseEntity.ok(savedCustomer);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return ResponseEntity.ok(customers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+        Customer customer = customerService.getCustomerById(id);
+        if (customer != null) {
+            return ResponseEntity.ok(customer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/exists")
+    public ResponseEntity<Boolean> checkCustomerExists(@PathVariable Long id) {
+        boolean exists = customerService.checkCustomerExists(id);
+        return ResponseEntity.ok(exists);
+    }
+}
